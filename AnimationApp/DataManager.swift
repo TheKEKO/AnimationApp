c92fed9677a08c95d008ba5339b1e14671300f58
Aleksandr F <a7214370@gmail.com>
2022-08-16 21:07:23 +0300

    Work with DataManager

--- AnimationApp/DataManager.swift
+++ AnimationApp/DataManager.swift
@@ -11,7 +11,36 @@ class DataManager {
     
     static let shared = DataManager()
     
-    let animations = Spring.AnimationPreset.allCases
+    let animations: [Spring.AnimationPreset] = [
+        .fadeIn,
+        .fadeInDown,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInUp,
+        .fadeOut,
+        .fadeOutIn,
+        .fall,
+        .flash,
+        .flipX,
+        .flipY,
+        .morph,
+        .pop,
+        .shake,
+        .slideDown,
+        .slideLeft,
+        .slideRight,
+        .slideUp,
+        .squeeze,
+        .squeezeDown,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeUp,
+        .swing,
+        .zoomIn,
+        .wobble,
+        .zoomOut
+    ]
+    
     let curves = Spring.AnimationCurve.allCases
     
     private init() {}
